a519f3fcb53a918861357bd6e0a40593e83c50eb
Arthur Debeaupte <arthuramerique@gmail.com>
2022-09-22 20:57:11 +0200

    adding role pages

--- app/pages/posts/consult.jsx
+++ app/pages/posts/consult.jsx
@@ -1,7 +1,12 @@
+import Layout from '../../components/layout';
+
+
 export default function Consult() {
     return (
-        <>
+        <Layout>
             <h1>Consult</h1>
-        </>
+
+
+        </Layout>
     );
 }
