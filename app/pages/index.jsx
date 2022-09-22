458dc36172381ca5402e39cb8ad4d18e66b3a8dc
Arthur Debeaupte <arthuramerique@gmail.com>
2022-09-22 16:51:29 +0200

    adding style

--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -1,16 +1,17 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-
+import Link from 'next/link';
+import Layout from '../components/layout';
 
 export default function Home() {
     return (
-        <>
+        <Layout>
             <h1>Welcome to ENSI Caen's library !</h1>
-            <Button variant="contained">CONSULT_ROLE</Button>
-            <Button variant="contained">BORROW_ROLE</Button>
-            <Button variant="contained">CONTRIBUTOR_ROLE</Button>
-            <Button variant="contained">ADMINISTRATOR_ROLE</Button>
-        </>
+            <Button variant="contained"><Link href="/posts/consult">CONSULT_ROLE</Link></Button>
+            <Button variant="contained"><Link href="/posts/borrow">BORROW_ROLE</Link></Button>
+            <Button variant="contained"><Link href="/posts/contributor">CONTRIBUTOR_ROLE</Link></Button>
+            <Button variant="contained"><Link href="/posts/administrator">ADMINISTRATOR_ROLE</Link></Button>
+        </Layout>
 
 )
 }
---
+++ app/pages/posts/consult.jsx
@@ -0,0 +1,7 @@
+export default function Consult() {
+    return (
+        <>
+            <h1>Consult</h1>
+        </>
+    );
+}
