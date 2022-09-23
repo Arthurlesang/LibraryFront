fc658ca97f6a62808b83c7aa1daaf733eb2e1a0f
Arthur Debeaupte <arthuramerique@gmail.com>
2022-09-23 23:47:06 +0200

    function to fetch data from REST API in consult.jsx page

--- app/pages/posts/consult.jsx
+++ app/pages/posts/consult.jsx
@@ -1,11 +1,30 @@
 import Layout from '../../components/layout';
+import axios from "axios";
+import react from "react";
+import {useState} from "react";
 
+function Consult() {
 
-export default function Consult() {
+    const [books, setBooks] = useState([]);
+    const fetchBooks = async () => {
+        const resposne = await fetch('');
+        const data = await response.json();
+        setBooks(data);
+    }
+
     return (
         <Layout>
             <h1>Consult</h1>
-
+            {
+                books.map(book => {
+                    return (
+                        <div key = {book.id}>
+                            {book.id} {book.title} {book.author} {book.publisher}
+                        </div>
+                    )
+                })
+            }
         </Layout>
     );
 }
+export default Consult;
