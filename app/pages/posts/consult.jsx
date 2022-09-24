f09b99899706b24b6c2a0da73a92c789a9ee5370
Arthur Debeaupte <arthuramerique@gmail.com>
2022-09-24 15:43:33 +0200

    fetching data from external API in consult.jsx page

--- app/pages/posts/consult.jsx
+++ app/pages/posts/consult.jsx
@@ -1,27 +1,26 @@
 import Layout from '../../components/layout';
 import {useState} from "react";
+import {NextResponse as response} from "next/server";
 
-function Consult() {
+export const getStaticProps = async() => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const data = await res.json();
 
-    const [books, setBooks] = useState([]);
-    const fetchBooks = async () => {
-        const resposne = await fetch('');
-        const data = await response.json();
-        setBooks(data);
+    return {
+        props : {books : data}
     }
+}
 
+const Consult = ({books}) => {
     return (
         <Layout>
             <h1>Consult</h1>
-            {
-                books.map(book => {
-                    return (
-                        <div key = {book.id}>
-                            {book.id} {book.title} {book.author} {book.publisher}
-                        </div>
-                    )
-                })
-            }
+
+            {books.map(book => (
+                <div key={book.name}>
+                    <h3>{book.name}</h3>
+                </div>
+            ))}
         </Layout>
     );
 }
