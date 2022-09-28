593db0e57c51e943a00c4cc85434756fc2d4554b
Arthur Debeaupte <arthuramerique@gmail.com>
2022-09-28 16:30:24 +0200

    trying to POST but not working

--- app/pages/posts/borrow.jsx
+++ app/pages/posts/borrow.jsx
@@ -2,8 +2,25 @@ import Layout from '../../components/layout';
 import Button from "@mui/material/Button";
 import {TextField} from "@mui/material";
 import * as React from "react";
+import {useState} from "react";
 
 const Borrow = () => {
+    const [user, setUser] = useState('');
+
+    const submitUser = async () => {
+        const response = await fetch('http://15.237.139.132:3000/book/add', {
+            method:'POST',
+            body: JSON.stringify({"title":"test",
+                "publishers":"/api/publisher/1",
+                "authors":[
+                    "/api/authors/1"]}),
+            headers:{'Content-Type' : 'application/json',
+            },
+        })
+        const data = await response.json();
+        console.log(data);
+    }
+
     return (
         <Layout>
             <h1>Borrow</h1>
@@ -12,6 +29,13 @@ const Borrow = () => {
             </div>
             <div>
                 <h2>Create new user : </h2>
+                <input type='text'
+                       value={user}
+                       onChange={e => setUser(e.target.value)}
+                />
+
+                <button onClick={submitUser}>Submit user</button>
+
                 <TextField
                     required
                     id="filled-required"
